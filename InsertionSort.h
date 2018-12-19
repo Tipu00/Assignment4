a673ec6b045ad59c4e083141e5d93710726ca7be
Taufikul islam <taufikul.islam@northsouth.edu>
2018-12-19 12:01:17 +0600

    Create header file of InsertionSort

---
+++ InsertionSort.h
@@ -0,0 +1,11 @@
+#ifndef INSERTIONSORT_H_INCLUDED
+#define INSERTIONSORT_H_INCLUDED
+class InsertionSort
+{
+    public:
+    InsertionSort();
+    ~InsertionSort();
+    void  InsertionSort(int arra[],int arra_size);
+    void InsertionSortVector(std::vector<int>& data)
+};
+#endif
