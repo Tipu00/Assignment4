c1a68ee6a4f42638b76f0eb100bfe217f42ac84e
Taufikul islam <taufikul.islam@northsouth.edu>
2018-12-19 14:02:38 +0600

    SelectionSort Using Vector

--- SelectionSort.cpp
+++ SelectionSort.cpp
@@ -13,9 +13,9 @@ inline void Swap(int &a, int &b)
     a = b;
     b = k;
 }
-void SelectionSort::SelectionSorter(int arr[], int arr_size)
+void SelectionSort::SelectioSorter(int arr[], int arr_size)
 {
-    cout <<"SelectionSort :"<<endl;
+    cout <<"SelectionSort Using Array:"<<endl;
     for(int i = 0; i < arr_size - 1; ++i)
     {
         int min = i;
@@ -31,3 +31,25 @@ void SelectionSort::SelectionSorter(int arr[], int arr_size)
     cout <<endl;
     }
 }
+	
+void SelectionSort::SelectioSorterVector(vector <int> &data)
+{
+    cout<<"Selection Sort Using Vector:"<<endl;
+     int index,num, first_element, size = data.size();
+
+     for ( index =0; index<size+1; index++ ){
+         first_element = index;
+         for ( num = index+1; num<size; num++ ){
+
+             if ( data[num] < data[first_element] )
+             first_element = num;
+         }
+     swap(data[first_element], data[index]);
+     for (std::vector<int>::iterator val = data.begin();val != data.end(); ++val)
+    std::cout << *val <<" " ;
+    cout<<""<<endl;
+     }
+    ;
+
+}
+
