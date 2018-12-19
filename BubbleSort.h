4fec6003bfaa307584862817a5e69965c52992f7
Taufikul islam <taufikul.islam@northsouth.edu>
2018-12-19 10:59:38 +0600

    Create header file

---
+++ BubbleSort.h
@@ -0,0 +1,11 @@
+#ifndef BUBBLESORT_H_INCLUDE
+#define BUBBLESORT_H_INCLUDE
+
+class BubbleSort
+{
+   public:
+     BubbleSort();
+    ~BubbleSort();
+    void BubbleSort(int *array, int n);
+};
+#endif
