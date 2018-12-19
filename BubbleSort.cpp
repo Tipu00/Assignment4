f10ed35f84b0791375e80a74b7ca59dddeed2c9b
Taufikul islam <taufikul.islam@northsouth.edu>
2018-12-19 11:45:26 +0600

    Bubble Sort Using Vector

--- BubbleSort.cpp
+++ BubbleSort.cpp
@@ -11,4 +11,28 @@ BubbleSort::~BubbleSort()
 Void BubbleSort::BubbleSort(int *array , int n);
 {
     cout<<"BubbleSort Using Array :"<<endl;
+    int num=0;
+    int temp;
+    bool swapped = true;
+
+    while(swapped)
+    {
+        swapped = false;
+        num++;
+        for(int index = 0; index<n; ++index)
+        {
+            if(array[index]>array[index+1])
+            {
+                temp = array[index];
+                array[index]=array [index+1];
+                array[index+1]=temp;
+                for(int index=0;index<8;index++)
+                {
+                    cout<<array[index]<<"";
+                }
+                cout<<""<<endl;
+                swapped = true;
+            }
+        }
+    }
 }
